b802763dc222dca8c1ee532cea06bcd5803225fe
Adeiza Ayo <adeizaayo@gmail.com>
2022-01-04 18:51:07 +0100

    update

--- api/utils.py
+++ api/utils.py
@@ -21,6 +21,24 @@ def tokenize(text):
 
 def load_final_model():
     with open('pipe.pickle', 'rb') as picklefile:
+        def tokenize(text):
+            from nltk.stem import WordNetLemmatizer
+            from nltk import word_tokenize
+
+            '''
+            PARAMETERS 
+                text (str): Text to be processed   
+            RETURN
+                Returns a processed text variable that was tokenized, lower cased, stripped, and lemmatized
+            '''
+            tokens = word_tokenize(text)
+            lemmatizer = WordNetLemmatizer()
+            clean_tokens = []
+            for tok in tokens:
+                clean_tok = lemmatizer.lemmatize(tok).lower().strip()
+                clean_tokens.append(clean_tok)
+
+            return clean_tokens
         saved_pipe = pickle.load(picklefile)
         
     return saved_pipe
